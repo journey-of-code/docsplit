cc909e4f31636a33c8b2477fccb69cf88efb3b31
Pascal Bertram <pascal@pixelmove.de>
2022-04-23 17:43:43 +0200

    Added tests and parameter handling for noop

--- docsplit.sh
+++ docsplit.sh
@@ -1,16 +1,31 @@
 #!/usr/bin/env bash
 
-# echo all commands before execution
-#set -o verbose
+VERSION="0.1"
+AUTHOR="Pascal Bertram"
 
 usage() {
 cat << EOF
-Usage: $(basename $0) input.pdf output
+Usage: $(basename $0) [options] input.pdf output
 Splits one large pdf into pieces.
+
+Options:
+  --help
+    Output this text.
+  --noop
+    Simulate running the script but don't do anything.
+
 'input.pdf' is the document to split.
 'ouput' will produce splits like 'output.00538.pdf'.
 The document number will be read from the files.
+
+Author: $AUTHOR
+Version: $VERSION
 EOF
+}
+
+version() {
+  executable=$(basename $0)
+  echo "${executable%\.sh} $VERSION"
 }
 
 # Failure safety
@@ -20,6 +35,8 @@ set -o nounset
 set -o errexit
 # if error exit is probable use
 # command || true
+# echo all commands before execution
+#set -o verbose
 
 # Check that all packages are installed that we need
 needed_packages=( ghostscript pdfgrep python3 )
@@ -36,13 +53,50 @@ install_help()
 ## Run the run_install function if any of the libraries are missing
 dpkg -s "${needed_packages[@]}" >/dev/null 2>&1 || install_help
 
+# Parse parameters
+# See http://stackoverflow.com/questions/402377/using-getopts-in-bash-shell-script-to-get-long-and-short-command-line-options
+# -o list of all single letter parameters (trailing ':' means it needs to have a value)
+# --long list of all long options (trailing ':' means it needs to have a value)
+# -n name of program to report
+{
+  TEMP=`getopt -o h \
+               --long help,noop,version \
+               -n 'docsplit.sh' -- "$@"`
+  # Defaults
+  PRINT_HELP=""
+  PRINT_VERSION=""
+  NOOP=""
+  # Break on errors and report correct usage.
+  if [ $? != 0 ] ; then echo "ERROR: Came accross a wrong option. Terminating..." >&2; usage; exit 1 ; fi
+  # Note the quotes around `$TEMP': they are essential!
+  eval set -- "$TEMP"
+  while true; do
+    case "$1" in
+      -h | --help ) PRINT_HELP=true; shift ;;
+      --noop ) NOOP=true; shift ;;
+      --version ) PRINT_VERSION=true; shift ;;
+      -- ) shift; break ;;
+      * ) break ;;
+    esac
+  done
+}
+
+# Print help if wanted and exit.
+[[ $PRINT_VERSION ]] && version && exit 0
+[[ $PRINT_HELP ]] && usage && exit 0
+
+# Checking assumptions
 # Check that we have two parameters given
-(( $# != 2 )) && usage && exit 0
+(( $# != 2 )) && usage && exit 1
 # Check that the first parameter is a file
 [[ ! -f $1 ]] && echo "The first parameter has to be a pdf that exists!" && usage && exit 1
 # Check that we wouldn't overwrite anything
-if compgen -G "$2"*.pdf > /dev/null; then
-  echo "No files with a name like '$2*.pdf' may exsist - they could be overwritten!" && usage && exit 1
+out_files=$(compgen -G "$2*.pdf") || true
+if [ -n "$out_files" ] >/dev/null ; then
+ echo "No files with a name like '$2*.pdf' may exist - they could be overwritten!"
+ echo "Found the following file(s):"
+ echo "$out_files"
+ usage && exit 1
 fi
 
 # Alias ghostscript so we don't have to be so verbose
@@ -52,7 +106,8 @@ gs_command() {
 
 # Find pages in the pdf
 # This constructs a python dict with page:found_number
-pages=$(pdfgrep -no -P '(?<=[^0-9]00)[0-9]{3}(?=[^0-9])' "$1" | tr '\n' ', ' | sed 's/^/{/'; echo "}")
+pages=$(pdfgrep -no -P '(?<=[^0-9]00)[0-9]{3}(?=[^0-9])' "$1" | tr '\n' ', ' | sed 's/^/{/'; echo "}") || true
+echo "$pages"
 # Create a command line for ghostscript
 result=$(python3 <<EOF
 def sliding_window(elements, window_size):
@@ -64,21 +119,21 @@ def qprint(first, last, indoc, outdoc, number):
   print(f"qpdf {indoc} --pages {indoc} {first}-{last} -- {outdoc}.{number:05d}.pdf")
 def gsprint(first, last, indoc, outdoc, number):
   if last:
-    last = f"-dLastPage={last}"
-  print(f"gs_command -dFirstPage={first} {last} -sOutputFile={outdoc}.{number:05d}.pdf {indoc}")
+    last = f" -dLastPage={last}"
+  print(f"gs_command -dFirstPage={first}{last} -sOutputFile={outdoc}.{number:05d}.pdf {indoc}")
 page_dict = $pages
 items = [(k,v) for k,v in page_dict.items()]
-# This would be for using qpdf (but that would not make the pdfs smaller)
-# for first, second in sliding_window(items,2):
-#   qprint(first[0], second[0]-1, "$1", "$2", first[1])
-# qprint(items[-1][0], "z", "$1", "$2", first[1])
 for first, second in sliding_window(items,2):
   gsprint(first[0], second[0]-1, "$1", "$2", first[1])
-gsprint(items[-1][0], "", "$1", "$2", first[1])
+gsprint(items[-1][0], "", "$1", "$2", items[-1][1])
 EOF
 )
 # This will execute the result from python line by line
 # See https://unix.stackexchange.com/a/181581
-echo "Processing PDFs..."
-eval "$result"
-echo "Finished!"
+if [[ $NOOP ]]; then
+  echo "$result"
+else
+  echo "Processing PDFs..."
+  eval "$result"
+  echo "Finished!"
+fi
--- test/test_docsplit.bats
+++ test/test_docsplit.bats
@@ -11,11 +11,62 @@ setup() {
   DIR="$( cd "$( dirname "$BATS_TEST_FILENAME" )" >/dev/null 2>&1 && pwd )"
   # make executables in src/ visible to PATH
   PATH="$DIR/../:$PATH"
+  # Create temp directory
+  # omit the -p parameter to create a temporal directory in the default location
+  TEST_DIR=$(mktemp -d -p "$DIR")
+  touch "$TEST_DIR/existing_file.pdf"
 }
 
-@test "Without parameters usage info is printed" {
+function teardown() {
+  # Delete stuff in test/output
+  rm -Rf "$TEST_DIR"
+  # a=1
+}
+
+@test "Parameter tests" {
+  usage_string='Usage: docsplit.sh [options] input.pdf output'
+  # script should fail if called without paramters
   run docsplit.sh
+  [[ $status -eq 1 ]]
+  assert_output --partial "$usage_string"
+  # script should not fail if called with help
+  run docsplit.sh -h
+  [[ $status -eq 0 ]]
+  assert_output --partial "$usage_string"
+  run docsplit.sh --help
+  [[ $status -eq 0 ]]
+  assert_output --partial "$usage_string"
+  # version should be printed
+  run docsplit.sh --version
+  [[ $status -eq 0 ]]
+  assert_output "docsplit 0.1"
+  # require two parameters
+  run docsplit.sh a
+  [[ $status -eq 1 ]]
+  run docsplit.sh a b c
+  [[ $status -eq 1 ]]
+}
+
+@test "Check file requirements" {
+  touch "$TEST_DIR/file1.pdf"
+  [ -e "$TEST_DIR/file1.pdf" ]
+  touch "$TEST_DIR/file2.pdf"
+  [ -e "$TEST_DIR/file2.pdf" ]
+  run docsplit.sh "$TEST_DIR/non_existing_file.pdf" "$TEST_DIR/file"
+  assert_output --partial 'The first parameter has to be a pdf that exists!'
+  [ "$status" -eq 1 ] # script should not fail if called with help
+  run docsplit.sh "$DIR/data/Simple.pdf" "$TEST_DIR/file"
+  assert_output --partial 'No files with a name like'
+  assert_output --partial 'file1.pdf'
+  assert_output --partial 'file2.pdf'
+  [ "$status" -eq 1 ] # script should not fail if called with help
+}
+
+@test "Check noop output" {
+  run docsplit.sh --noop "$DIR/data/Simple.pdf" "$TEST_DIR/file"
   [ "$status" -eq 0 ]
-  assert_output --partial 'Usage: docsplit.sh input.pdf output'
-  assert_output --partial 'Splits one large pdf into pieces.'
+  assert_line --regexp 'gs_command -dFirstPage=1 -dLastPage=1.*file.00123.pdf'
+  assert_line --regexp 'gs_command -dFirstPage=2 -dLastPage=3.*file.00124.pdf'
+  assert_line --regexp 'gs_command -dFirstPage=4 -sOutput.*file.00125.pdf'
+  # if [ -z "$(ls -A /path/to/dir)" ]; then
 }
